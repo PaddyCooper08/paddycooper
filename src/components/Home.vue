1ef5f973f5d7e7d9de9667d89bd3ed48cfa376cb
Paddy Cooper <1210@rgsg.co.uk>
2022-09-05 19:16:55 +0100

    first code item

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-[1vh] text-center bg-[#122738]">
-    <h1 id="typewriter" class="text-5xl p-5 text-[#ffc600]"></h1>
+  <div class="h-[1vh] bg-[#122738] text-center">
+    <h1 id="typewriter" class="p-5 text-5xl text-[#ffc600]"></h1>
   </div>
   <div class="flex h-[99vh] items-center justify-center bg-[#122738]">
     <div
@@ -28,40 +28,41 @@
       >
         CMD
       </div>
-      <div class="p-6">
-        <h5 class="mb-2 text-xl font-medium text-gray-900">
-          Special title treatment
-        </h5>
-        <p class="mb-4 text-base text-gray-700">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <button
-          type="button"
-          class="
-            inline-block
-            rounded
-            bg-blue-600
-            px-6
-            py-2.5
-            text-xs
-            font-medium
-            uppercase
-            leading-tight
-            text-white
-            shadow-md
-            transition
-            duration-150
-            ease-in-out
-            hover:bg-blue-700 hover:shadow-lg
-            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-            active:bg-blue-800 active:shadow-lg
-          "
-        >
-          Button
-        </button>
-      </div>
-      <div class="border-t border-gray-300 py-3 px-6 text-gray-600">
-        2 days ago
+      <div class="py-6">
+        <div class="flex w-full items-center">
+          <h5 class="float-left text-left px-5 text-2xl text-[#ff0088]">
+            ~ paddy@cooper/info/about ->
+          </h5>
+          <h5 class="pl-5 pr-10 text-left text-xl text-[#ffc600]">
+            <p id="about"></p>
+            <p>
+              <a href="/about" class="underline">localhost:3000/about</a>
+            </p>
+          </h5>
+          <!-- <button
+            type="button"
+            class="
+              float-left
+              inline-block
+              rounded
+              border-2 border-[#ffc600]
+              px-10
+              py-2.5
+              text-xs
+              font-medium
+              uppercase
+              leading-tight
+              text-[#ff0088]
+              transition
+              duration-150
+              ease-in-out
+              hover:bg-black hover:bg-opacity-5
+              focus:outline-none focus:ring-0
+            "
+          >
+            Enter
+          </button> -->
+        </div>
       </div>
     </div>
   </div>
@@ -93,6 +94,10 @@ export default {
       .pauseFor(450)
 
       .start();
+    var aboutTypewriter = new Typewriter("#about", {
+      autoStart: true,
+    });
+    aboutTypewriter.pasteString("cat ./about.link ").start();
   },
 };
 </script>
