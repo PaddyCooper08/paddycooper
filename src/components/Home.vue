b493f17a0a4e23a1d5bbd91e8045a17f49cac234
Paddy Cooper <1210@rgsg.co.uk>
2022-09-09 21:50:05 +0100

    Moved title into it's own component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,4 @@
 <template>
-  <div class="h-[1vh] bg-[#122738] text-center">
-    <h1 id="typewriter" class="p-5 text-5xl text-[#ffc600]"></h1>
-  </div>
   <div class="flex h-[99vh] items-center justify-center bg-[#122738]">
     <div
       class="
@@ -69,36 +66,5 @@
 </template>
 
 <script>
-import Typewriter from "typewriter-effect/dist/core";
-export default {
-  mounted() {
-    var typewriter = new Typewriter("#typewriter", {
-      autoStart: true,
-    });
-    typewriter
-      .typeString(">Paddy Cooper - Developer")
-      .pauseFor(450)
-      .deleteChars(9)
-      .typeString("Problem Solver")
-      .pauseFor(450)
-      .deleteChars(14)
-      .pauseFor(50)
-      .typeString("Designer")
-      .pauseFor(450)
-      .deleteChars(8)
-      .pauseFor(50)
-      .typeString("Entrepreneur")
-      .pauseFor(450)
-      .deleteChars(12)
-      .typeString("Legend 💪")
-      .pauseFor(450)
-
-      .start();
-    var aboutTypewriter = new Typewriter("#about", {
-      autoStart: true,
-    });
-    aboutTypewriter.pasteString("cat ./about.link ").start();
-  },
-};
 </script>
 
