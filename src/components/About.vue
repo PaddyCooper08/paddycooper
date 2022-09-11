88ba6496976157189bfe92848db0aead2aaf5fc1
Paddy Cooper <1210@rgsg.co.uk>
2022-09-11 19:19:17 +0100

    Added about page

---
+++ src/components/About.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="flex h-[99vh] items-center justify-center bg-[#122738]">
+    <div
+      class="
+        mx-auto
+        my-auto
+        h-[80%]
+        w-[60%]
+        rounded-lg
+        bg-[#193549]
+        text-center
+        shadow-lg
+        only:block
+        relative
+        items-center
+      "
+    >
+      <div
+        class="
+          border-b-2 border-[#ffc600]
+          py-3
+          px-6
+          text-xl
+          font-bold
+          text-[#ff0088]
+        "
+      >
+        CMD
+      </div>
+      <div class="p-12 text-xl">
+        <p class="text-white font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+          deserunt dolorum, nulla harum deleniti, officiis nisi perferendis ad
+          sit, voluptatem tenetur cupiditate minima at debitis in quisquam
+          sapiente itaque. Autem laboriosam atque facere optio excepturi.
+          Laudantium facere earum aliquam qui, voluptatum quas accusantium
+          itaque ducimus consequatur debitis incidunt minima inventore nisi
+          commodi neque iste aperiam nulla adipisci enim magni architecto iure?
+          Asperiores iusto consequatur impedit aliquam sequi necessitatibus,
+          beatae placeat nam dicta. Voluptas atque soluta veniam voluptatem
+          dolorem nemo saepe illum reprehenderit eveniet doloribus quasi nam,
+          aspernatur quos ratione reiciendis impedit inventore! Vel rerum
+          consectetur, odio voluptatem magnam doloremque sed.
+        </p>
+      </div>
+      <a
+        href="/"
+        class="
+          inline-block
+          text-[#ff0088] text-3xl
+          absolute
+          bottom-0
+          text-center
+          right-[40%]
+          mx-auto
+          py-6
+        "
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="#ff0088"
+          class="bi bi-arrow-bar-left inline-block"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"
+          />
+        </svg>
+        take me home
+      </a>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap");
+* {
+  font-family: "Open Sans", sans-serif;
+}
+</style> 
---
+++ src/components/AboutTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-[1vh] bg-[#122738] text-center">
+    <h1 id="typewriter" class="p-5 text-5xl text-[#ffc600]"></h1>
+  </div>
+</template>
+
+<script>
+import Typewriter from "typewriter-effect/dist/core";
+export default {
+  mounted() {
+    var typewriter = new Typewriter("#typewriter", {
+      autoStart: true,
+    });
+    typewriter
+      .typeString(">About")
+
+      .start();
+  },
+};
+</script>
+
+<style>
+</style>
