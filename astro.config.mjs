f34596d75c89948ccb7b4f6eb6a0c201e42382e1
Paddy Cooper <1210@rgsg.co.uk>
2023-02-05 20:08:35 +0000

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
+  site: 'https://paddycooper.github.io',
+  base: '/paddycooper',
 
 
 });
